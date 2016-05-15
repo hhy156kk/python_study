91aaf30196767418e6339de722522771c7c92747
hhy156kk <hhy156kk@163.com>
2016-05-15 21:17:28 +0800

    spider 0.2a

--- spider.py
+++ spider.py
@@ -1,13 +1,9 @@
 import urllib2
-import urllib
 
-data={}
-data['word']='Jecvay Notes'
-
-url_values = urllib.urlencode(data)
-url = "http://www.baidu.com/s?"
-full_url = url + url_values
+values = {"username":"385708813@qq.com",}
 
 
-response = urllib2.urlopen(full_url)
+request = urllib2.Request("http://www.baidu.com")
+response = urllib2.urlopen(request)
+
 print response.read()
