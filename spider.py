3fadcf38a4732e833e190386ebc1a701a13a2a8e
hhy156kk <hhy156kk@163.com>
2016-05-17 23:16:37 +0800

    spider 0.3

--- spider.py
+++ spider.py
@@ -1,9 +1,16 @@
 import urllib2
+import urllib
 
-values = {"username":"385708813@qq.com",}
+url = 'http://www.server.com/login'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 
+values = {"username":"385708813@qq.com", "password":"hhy156kk"}
+headers = {'User-Agent':user_agent,'Referer':'http://www.zhihu.com/articles'}
 
-request = urllib2.Request("http://www.baidu.com")
+data = urllib.urlencode(values)
+
+request = urllib2.Request(url,data,headers)
+
 response = urllib2.urlopen(request)
 
 print response.read()
