35288c6fc1f0eccc68eccf66f97c32c5ebfcd8af
hhy156kk <hhy156kk@163.com>
2016-05-23 23:31:02 +0800

    spider 0.8a

--- spider.py
+++ spider.py
@@ -8,12 +8,16 @@ url = 'http://www.qiushibaike.com/hot/page/' + str(page)
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = {'User-Agent': user_agent}
 
-request = urllib2.Request(url, headers=headers)
-response = urllib2.urlopen(request)
-
-content = response.read().decode('utf-8')
-print content
-pattern = re.compile(r'<div.*?class="content">(.*?)</div>', re.S)
-items = re.findall(pattern, content)
-for item in items:
-    print item
+try:
+    request = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(request)
+    content = response.read().decode('utf-8')
+    pattern = re.compile('<div.*?class="content">(.*?)</div>', re.S)
+    items = re.findall(pattern, content)
+    for item in items:
+        print item
+except urllib2.URLError,e:
+    if hasattr(e,"code"):
+        print e.code
+    if hasattr(e,"reason"):
+        print e.reason
