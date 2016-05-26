e3f87096fc899d21c849af16ec4ad5e39b22b975
hhy156kk <hhy156kk@163.com>
2016-05-26 23:57:03 +0800

    0.9a

--- spider.py
+++ spider.py
@@ -4,56 +4,70 @@ import urllib2
 import re
 
 class QSBK_Spider:
+
     def __init__(self):
         self.pageIndex = 1
         self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-        self.headers = {'User-Agent':self.user_agent}
+        self.headers = {'User-Agent' : self.user_agent}
         self.stories = []
         self.enable = False
 
+
+
     def getPage(self,pageIndex):
-        url = 'http://www.qiushibaike.com/hot/page/' + str(pageIndex)
-        request = urllib2.Request(url,headers=self.headers)
-        response = urllib2.urlopen(request)
-        return response.read().decode('utf-8')
+        try:
+            url = 'http://www.qiushibaike.com/hot/page/' + str(pageIndex)
+            request = urllib2.Request(url,headers=self.headers)
+            response = urllib2.urlopen(request)
+            pageCode = response.read().decode('utf-8')
+            return pageCode
+        except urllib2.URLError,e:
+            if hasattr(e,"reason"):
+                print u"连接糗事百科失败,错误原因",e.reason
+                return None
+
 
     def getPageItems(self,pageIndex):
         pageCode = self.getPage(pageIndex)
-        pattern = re.compile('<div.*?class="content">(.*?)</div>',re.S)
+        if not pageCode:
+            print "页面加载失败..."
+            return None
+        pattern = re.compile(r'<div.*?class="content">(.*?)</div>',re.S)
         items = re.findall(pattern,pageCode)
         pageStories = []
         for item in items:
             pageStories.append(item)
-        return pageStories
+        return  pageStories
 
-   def loadPage(self):
-       if self.enable == True:
-           if len(self.stories)<2:
-               pageStories = self.getPageItems(self.pageIndex)
-               self.stories.append(pageStories)
-               self.pageIndex += 1
+    def loadPage(self):
+        if self.enable == True:
+            if len(self.stories) < 2:
+                pageStories = self.getPageItems(self.pageIndex)
+                if pageStories:
+                    self.stories.append(pageStories)
+                    self.pageIndex += 1
 
-
-    def getOneStory(self,pageStories,page):
+    def getOneStory(self,pageStories):
         for story in pageStories:
             input = raw_input()
             self.loadPage()
-            if input == 'Q'
+            if input == "Q":
                 self.enable = False
                 return
-            print
+            print story
+
+
 
     def start(self):
-        print u'正在读取糗事百科,按回车查看新段子，Q退出'
+        print u"正在读取糗事百科,按回车查看新段子，Q退出"
         self.enable = True
         self.loadPage()
-        nowPage = 0
         while self.enable:
             if len(self.stories)>0:
                 pageStories = self.stories[0]
-                nowPage += 1
                 del self.stories[0]
-                self.getOneStory(pageStories,nowPage)
+                self.getOneStory(pageStories)
+
 
 spider = QSBK_Spider()
 spider.start()
