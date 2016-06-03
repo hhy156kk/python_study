82cfb6c1523f8c0cbcab17b8ae975449e97944f2
hhy156kk <hhy156kk@163.com>
2016-06-03 21:14:39 +0800

    BaiduSpider 1.0a

--- BaiduSpider.py
+++ BaiduSpider.py
@@ -20,32 +20,26 @@ class Tool:
 
 class BDTB:
 
-    def __init__(self,baseUrl,seeLZ):
+    def __init__(self,baseUrl,seeLZ,pn):
         self.baseURL = baseUrl
         self.seeLZ = '?see_lz=' + str(seeLZ)
+        self.pageIndex ='&pn='+str(pn)
         self.tool = Tool()
 
-    def getPage(self,pageNum):
-        try:
-            url = self.baseURL + self.seeLZ
+    def getPage(self,url):
             request = urllib2.Request(url)
             response = urllib2.urlopen(request)
             return response.read().decode('utf-8')
-        except urllib2.URLError,e:
-            if hasattr(e,"reason"):
-                print u"百度贴吧链接失败，错误原因"+e.reason
-                return None
-    def getTitle(self):
-        page = self.getPage(1)
+
+    def getTitle(self,page):
         pattern = re.compile(r"<h3.*?>(.*?)</h3>",re.S)
         result = re.search(pattern,page)
         if result:
-            return result.group(1).strip()
+            return result.group(1).strip().encode('utf-8')
         else:
             return None
 
-    def getPageNum(self):
-        page = self.getPage(1)
+    def getPageNum(self,page):
         pattern = re.compile(r'<li class="l_reply_num.*?</span>.*?<span class="red">(.*?)</span>',re.S)
         result = re.search(pattern,page)
         if result:
@@ -53,23 +47,29 @@ class BDTB:
         else:
             return None
 
-    def getContent(self):
-        page = self.getPage(1)
+    def getContent(self,page):
         pattern = re.compile(r'<div id="post_content_.*?>(.*?)</div>',re.S)
         items = re.findall(pattern,page)
         contents = []
         for item in items:
             content = "\n" + self.tool.replace(item) + "\n"
             contents.append(content.encode('utf-8'))
-            print content
         return contents
-    def start(self):
-        indexPage = self.getPage(1)
-        pageNum = self.getPageNum(indexPage)
-        title = self.getTitle(indexPage)
 
-baseUrl = 'http://tieba.baidu.com/p/3138733512'
-bdtb = BDTB(baseUrl,1)
-#bdtb.getTitle()
-#bdtb.getPageNum()
-bdtb.getContent()
+    def start(self):
+        url = self.baseURL + self.seeLZ + self.pageIndex
+        page = self.getPage(url)
+        title = self.getTitle(page)
+        pageNum = self.getPageNum(page)
+        contents = self.getContent(page)
+        print "帖子标题: " + str(title)
+        print "帖子页数：" + str(pageNum)
+        for content in contents:
+            print content
+
+baseURL = "http://tieba.baidu.com/p/3138733512?"
+seeLZ = raw_input("是否之收看楼主")
+pn = raw_input("帖子页码")
+
+bdtbSpider = BDTB(baseURL,seeLZ,pn)
+bdtbSpider.start()
